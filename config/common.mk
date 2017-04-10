392827b59aa146c7f47a83110439838b9c79fd16
sandstranger <sandstranger@gmail.com>
2017-04-10 20:53:23 +0400

    update

--- config/common.mk
+++ config/common.mk
@@ -58,10 +58,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libem_sensor_jni
 
-# Bootanimation
-PRODUCT_COPY_FILES += \
-    vendor/mad/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
-
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # Tethering
