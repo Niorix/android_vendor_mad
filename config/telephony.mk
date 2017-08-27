784ec23dd70a7278622626f2b59a35c7e45f7ab9
sandstranger <sandstranger@gmail.com>
2017-08-27 22:22:39 +0400

    remove apn xml

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,6 +1,5 @@
 # Configs
 PRODUCT_COPY_FILES += \
-    vendor/mad/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml \
     vendor/mad/prebuilt/etc/ecc_list.xml:system/etc/ecc_list.xml \
     vendor/mad/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
 
